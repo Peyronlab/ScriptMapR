19ab250392fc2c0b7c1bf94dfd3bf291a6cd32cc
raph06 <raphael.bonnet.06@gmail.com>
2020-09-24 09:32:56 +0200

    correct RCy3 no view error atmp6-netreemoval

--- R/scriptmapr.R
+++ R/scriptmapr.R
@@ -2032,10 +2032,10 @@ scriptmapr = function(path) {
   title = paste(script_name, as.POSIXct(Sys.time()))
   style.name = gsub(".R", "", basename(script_name))
   style.name = "ScriptMapR"
-  ids = getNetworkList()[which(grepl(script_name, getNetworkList(), perl = TRUE))]
-  lapply(seq_along(ids), function(x) {
-    deleteNetwork(ids[x])
-  })
+  # ids = getNetworkList()[which(grepl(script_name, getNetworkList(), perl = TRUE))]
+  # lapply(seq_along(ids), function(x) {
+    # deleteNetwork(ids[x])
+  # })
 
   #reset default style
   commandsPOST("vizmap apply styles=\"default\"")
